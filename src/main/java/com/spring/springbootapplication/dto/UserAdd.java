bb04a4b3eb15b7366294105499d8a5a714655a36
masaki-nakamoto <masaki.nakamoto@prum.jp>
2025-09-02 23:44:00 +0900

    fix validation

--- src/main/java/com/spring/springbootapplication/dto/UserAdd.java
+++ src/main/java/com/spring/springbootapplication/dto/UserAdd.java
@@ -13,17 +13,17 @@ import jakarta.validation.constraints.Pattern;
 public class UserAdd implements Serializable {
 
   @NotBlank(message = "氏名は必ず入力してください")
-  @Size(max = 15, message = "氏名は255文字以内で入力してください")
+  @Size(max = 255, message = "氏名は255文字以内で入力してください")
   private String name;
 
   @NotBlank(message = "メールアドレスは必ず入力してください")
-  @Size(max = 50, message = "メールアドレスは255文字以内で入力してください")
+  @Size(max = 255, message = "メールアドレスは255文字以内で入力してください")
   @Pattern(regexp = "^[\\w!#%&'/=~`\\*\\+\\?\\{\\}\\^\\$\\-\\|]+(\\.[\\w!#%&'/=~`\\*\\+\\?\\{\\}\\^\\$\\-\\|]+)*@[\\w!#%&'/=~`\\*\\+\\?\\{\\}\\^\\$\\-\\|]+(\\.[\\w!#%&'/=~`\\*\\+\\?\\{\\}\\^\\$\\-\\|]+)*$", message = "メールアドレスが正しい形式ではありません")
   private String email;
 
   @NotBlank(message = "パスワードは必ず入力してください")
   @Size(min = 8, max = 24, message="8文字以上24文字以内で入力してください")
-  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z]).*$", message = "英数字8文字以上で入力してください")
+  @Pattern(regexp = "^(?=.*[a-z]).*$", message = "英数字8文字以上で入力してください")
   private String password;
 
 
